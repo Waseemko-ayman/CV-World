3c3b6fe9106e9233eef5d43b9770c5f89868a40a
Waseem A. Abd Elhadi <112069422+Waseemko-ayman@users.noreply.github.com>
2023-02-26 20:11:50 +0200

    Modifie Js File (Progress Section)

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -3,7 +3,7 @@ function redirectMe(url) {
 }
 
 /***********************************************************************/
-
+// Header And Button Up
 let myButton = document.getElementById("group");
 let header = document.getElementById("header");
 
@@ -35,4 +35,43 @@ menuDetails.onclick = function () {
     } else {
         menu.style.display = "block";
     }
+}
+
+/***********************************************************************/
+
+// Progrss Section
+let section = document.querySelector("#our-skills");
+let spanProgrss = document.querySelectorAll("#our-skills span");
+
+window.onscroll = function () {
+    if (window.scrollY >= section.offsetTop - 300) {
+        spanProgrss.forEach((span) => {
+            span.style.width = span.dataset.width;
+        });
+    };
+};
+
+/***********************************************************************/
+
+let statsSection = document.querySelector(".stats");
+let numStat = document.querySelectorAll(".awesome .number");
+let started = false;
+
+window.onscroll = function () {
+    if (window.scrollY >= statsSection.offsetTop - 300) {
+        if (!started) {
+            numStat.forEach((num) => startCount(num));
+        }
+        started = true;
+    }
+}
+
+function startCount(el) {
+    let goal = el.dataset.goal;
+    let count = setInterval(() => {
+        el.textContent++;
+        if (el.textContent == goal) {
+            clearInterval(count);
+        }
+    }, 2000 / goal); // قسمتها علشان ينتهوا مع بعض
 }
